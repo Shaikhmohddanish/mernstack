41b730aadcbdb5589a92221c3b9ac6898a1ce3b3
Mohd Danish Shaikh <shaikhmohddanish>
2025-07-09 21:09:55 +0530

    Added

---
+++ Javascript/script.js
@@ -0,0 +1,69 @@
+// Operators
+
+// = - Assignment operators
+
+// +, - , / , *
+
+// let a = 10;
+// let b = 20;
+// let c = a+b;
+// console.log("a + b =",c);
+// console.log("a - b =",a-b);
+// console.log(a*b); 
+// astrick
+
+// console.log(a/b);
+
+// let x = 10;
+// let y = 3;
+// let answer = x%y;
+// console.log(answer);
+
+// console.log(4**5);
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a);
+
+// a = a + 1;
+// a++;
+
+// a--;
+// a = a-1;
+
+// console.log(a);
+
+let a = 3;
+console.log(a);
+
+// a += 5;
+// a = a + 5;
+// a -= 10;
+// a = a - 10;
+
+// a *= 10; 
+
+// a = a * 10;
+
+// a /= 10;
+// a = a / 10;
+
+// a %= 10;
+// a = a % 10;
+
+// a **= 3;
+
+a = a**3;
+
+console.log(a);
+
+
+
+
+
+
+
+
+
+
